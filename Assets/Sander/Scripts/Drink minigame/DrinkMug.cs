b7041d8d9ef5368e8e4bae647f8aa18c13f7cafb
Sander Y B <sanderybrinkman@live.nl>
2021-05-31 14:11:06 +0200

    Poem scene small Ui update

--- Assets/Sander/Scripts/Drink minigame/DrinkMug.cs
+++ Assets/Sander/Scripts/Drink minigame/DrinkMug.cs
@@ -98,6 +98,5 @@ public class DrinkMug : MonoBehaviour
         print("FillMug done!!");
     }
 
-    
 
 }
--- Assets/Sander/Scripts/Drink minigame/TapScript.cs
+++ Assets/Sander/Scripts/Drink minigame/TapScript.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class TapScript : MonoBehaviour
 {
     public GameObject fillFX;
+    public float collisonCoolDownTime = 0.5f;
 
     [Tooltip("make sure this is the same as the index of the corresponding 'drinkType' in the 'DrinkGameManager'")]
     public int drinkId = 1;
     //make sure this is the same as the index of the corresponding "drinkType" in the 'DrinkGameManager'
     //0 is used for empty parts in the mug and should never be used
+
+    private IEnumerator CollisionCoolDown()
+    {
+        yield return new WaitForSeconds(4);
+    }
 }
