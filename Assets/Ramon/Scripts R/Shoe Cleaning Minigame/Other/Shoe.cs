920c6ccae3a1af3888b284abfb1d62db1454b1de
KoningSherwood <ramonzwiep@hotmail.com>
2021-06-25 15:04:12 +0200

    Shoe Minigame Bugfix 3.0

--- Assets/Ramon/Scripts R/Shoe Cleaning Minigame/Other/Shoe.cs
+++ Assets/Ramon/Scripts R/Shoe Cleaning Minigame/Other/Shoe.cs
@@ -32,16 +32,10 @@ public class Shoe : MonoBehaviour
     {
         if (Manager.manager.shoeManager.GetComponent<HoldTool>().thirdTool.activeSelf && Input.GetMouseButton(mouseButton))
         {
-            Debug.Log("Shoe OnMouseOver 1");
-
             if (dirtCount <= minimumCleanliness)
             {
-                Debug.Log("Shoe OnMouseOver 2");
-
                 if (Time.time >= oncePerSecond)
                 {
-                    Debug.Log("Shoe OnMouseOver 3");
-
                     oncePerSecond = Time.time + cleanTime;
 
                     cleanliness -= cleanAmount;
